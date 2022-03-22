4ca6aff796e487ed73d552a213bb8e3bc9014fee
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-22 15:53:30 -0400

    finish the function to getUserRepos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user){
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+    //asynchronous behavior: set aside the fetch request and continue to implement the rest of the code.
 };
 
-getUserRepos();
+getUserRepos("hfang821");
